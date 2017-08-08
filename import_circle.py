e9d5f48431ebb5bb7fc4084aec13eed4861521e9
Aishwarya Sharma <aishwaryasharma@Aishwaryas-MacBook-Pro.local>
2017-08-07 21:27:16 -0400

    initial setup

---
+++ import_circle.py
@@ -0,0 +1,4 @@
+from math import pi
+radius = input('Please enter the circle radius: ')
+print('The area of circle is', float(radius) ** 2 * pi)
+print('The circumference of circle is', 2 * pi * float(radius))
